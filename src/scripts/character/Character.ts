32f1dc2ce0e0b4e9bbb57b7a44e3eb5f1ccab8c1
Josie Thompson <dot.slash.josie@gmail.com>
2023-11-11 21:07:17 -0800

    added debug for interaction rect overlap

--- src/scripts/character/Character.ts
+++ src/scripts/character/Character.ts
@@ -1,4 +1,6 @@
 export default class Character extends Phaser.Physics.Arcade.Sprite {
+    static interactionPadding: number = 100;
+    interactionBounds: Phaser.GameObjects.Rectangle;
 
     constructor(scene: Phaser.Scene,
                 characterId: string, x: number, y: number) {
@@ -6,5 +8,14 @@ export default class Character extends Phaser.Physics.Arcade.Sprite {
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.setImmovable();
+
+        this.interactionBounds = scene.add.rectangle(
+            x, y,
+            this.displayWidth + Character.interactionPadding,
+            this.displayHeight + Character.interactionPadding
+        );
+        scene.physics.add.existing(this.interactionBounds);
+        this.interactionBounds.setStrokeStyle(1, 0xeb4034);
+        this.interactionBounds.setDepth(-1);
     }
 }
--- src/scripts/character/CharacterSystem.ts
+++ src/scripts/character/CharacterSystem.ts
@@ -2,18 +2,27 @@ import Character from '../character/Character'
 
 export default class CharacterSystem extends Phaser.GameObjects.GameObject {
     characters: Phaser.GameObjects.Group;
+    interactionBounds: Phaser.GameObjects.Group;
 
     constructor(scene: Phaser.Scene, player: Phaser.GameObjects.GameObject) {
         super(scene, 'CharacterSystem');
         this.characters = scene.add.group();
+        this.interactionBounds = scene.add.group();
         scene.physics.add.collider(player, this.characters);
+        scene.physics.add.overlap(
+            player, this.interactionBounds,
+            (obj1, obj2) => {
+                console.log("overlap!");
+            });
     }
 
     add(scene: Phaser.Scene, characterId: string,
         x: number, y: number) {
 
-        this.characters.add(new Character(
+        let character = new Character(
             scene, characterId, x, y
-        ));
+        );
+        this.characters.add(character);
+        this.interactionBounds.add(character.interactionBounds);
     }
 }
