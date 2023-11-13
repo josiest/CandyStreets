23b069517278e2582a71f0e2e9b0f53c0bef175a
s <11468529+helioscope@users.noreply.github.com>
2023-11-13 11:01:13 -0800

    adjusted standalone image preload filepaths to match origin/main (using temp folder)

--- tiled/LooseImages.tsx
+++ tiled/LooseImages.tsx
@@ -5,21 +5,21 @@
  </editorsettings>
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="513" height="429" source="pirate-house.png"/>
+  <image width="513" height="429" source="temp/pirate-house.png"/>
  </tile>
  <tile id="1">
-  <image width="505" height="362" source="spider-house.png"/>
+  <image width="505" height="362" source="temp/spider-house.png"/>
  </tile>
  <tile id="2">
-  <image width="678" height="548" source="spooky-house.png"/>
+  <image width="678" height="548" source="temp/spooky-house.png"/>
  </tile>
  <tile id="3">
-  <image width="128" height="128" source="candy-lama.png"/>
+  <image width="128" height="128" source="temp/candy-lama.png"/>
  </tile>
  <tile id="4">
-  <image width="128" height="128" source="candy-pirate.png"/>
+  <image width="128" height="128" source="temp/candy-pirate.png"/>
  </tile>
  <tile id="5">
-  <image width="128" height="128" source="candy-cat.png"/>
+  <image width="128" height="128" source="temp/candy-cat.png"/>
  </tile>
 </tileset>
